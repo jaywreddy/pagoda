3850e46774ca3538430d07aeae3c58c241063e30
jaywreddy <jay.w.reddy@gmail.com>
2015-07-22 06:33:21 +0000

    new mount

--- mount.py
+++ mount.py
@@ -1,4 +1,5 @@
 from cadquery import *
+
 def circlemount():
     r = Workplane("front").circle(2.0)                       # make base
     r = r.pushPoints( [ (1.5,0),(0,1.5),(-1.5,0),(0,-1.5) ] )     # now four points are on the stack
@@ -6,6 +7,21 @@ def circlemount():
     result = r.extrude(0.125 )                               # make prism
     return result
 
-with open('ex.stl','w') as f:
-    f.write(exporters.toString(circlemount(),'STL'))
+def solenoid():
+    wp = Workplane("front") #work face
+    body = wp.box(2.0, 2.0, 8)
+    bFace = body.faces(">Z")      
 
+    nozzle = wp.circle(2.0).extrude(2) #inner  
+    
+   # body =  
+
+def export(name, shape):
+    with open(name,'w') as f:
+        f.write(exporters.toString(shape, 'STL'))
+
+def main():
+    export("ex.stl", circlemount())
+
+if  __name__ == "__main__":
+    main()
